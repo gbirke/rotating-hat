0a3f9943331aad26ee622f9a43adfc53ba315ca5
Gabriel Birke <gabriel.birke@gmail.com>
2017-10-08 14:24:59 +0200

    Fix jQuery not being loaded

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,11 @@
+// external CSS libraries
 import 'bootstrap';
 import 'bootstrap-datepicker';
 //import 'bootstrap/scss/bootstrap.scss'
 
+// CSS files
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.css';
 
+// Our code
 import './main';
